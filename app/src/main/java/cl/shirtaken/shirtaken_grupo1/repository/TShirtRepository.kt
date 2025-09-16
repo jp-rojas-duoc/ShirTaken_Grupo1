b51d9ac59cbabb87d3314da79c22e4c3b3357065
jp-rojas-duoc <jp.rojasg@duocuc.cl>
2025-09-16 11:49:10 -0300

    Commit inicial, agregada homescreen.kt para mostrar logo de aplicación

---
+++ app/src/main/java/cl/shirtaken/shirtaken_grupo1/repository/TShirtRepository.kt
@@ -0,0 +1,7 @@
+package cl.shirtaken.shirtaken_grupo1.repository
+
+import cl.shirtaken.shirtaken_grupo1.model.TShirt
+
+class TShirtRepository {
+    fun getCatalog(): List<TShirt> = emptyList()
+}
